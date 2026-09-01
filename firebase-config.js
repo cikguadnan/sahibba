d37a373090f69d04a91e70948f06275657d5faef
cikguadnan <adnan_mahmud@moe.edu.sg>
2026-09-01 23:25:24 +0800

    Configure Firebase for Google teacher login

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-window.SAHIBBA_FIREBASE_CONFIG = { apiKey: "", authDomain: "", projectId: "", storageBucket: "", messagingSenderId: "", appId: "" };
+window.SAHIBBA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDzwDMcJhsHDMap6fBSsr7Az_tI2cOIggg",
+  authDomain: "sahibba.firebaseapp.com",
+  projectId: "sahibba",
+  storageBucket: "sahibba.firebasestorage.app",
+  messagingSenderId: "834942741013",
+  appId: "1:834942741013:web:6df80cfebcfd6e31347c1c"
+};
